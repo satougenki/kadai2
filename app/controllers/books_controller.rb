c300ca2d4b72946c00a13a10a6c87a2171287a69
EC2 Default User <ec2-user@ip-172-31-43-171.ap-northeast-1.compute.internal>
2023-01-09 04:50:54 +0000

    「Finsh」1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,10 +25,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."    
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+   else 
+   # flash.now[:alert] = "error" 
+    render :edit
+   end    
   end
   
    def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
   get '/top' => 'homes#top'
   root to: 'homes#top'
-  patch 'book/:id' => 'books#update' , as: 'update_book'
+  patch 'books/:id' => 'books#update' , as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   delete 'books/:id' => 'books#destroy' ,as: 'destroy_book'
 end
